68b055fdc75c7acf985a1b742dbc39e5406a4dc4
Suraj Vijay <surajcpp115@gmail.com>
2024-06-29 12:55:51 +0530

    updated configuration.rs to use new settings

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,9 +1,7 @@
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::{
-    postgres::{PgConnectOptions, PgSslMode},
-    ConnectOptions,
-};
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -29,26 +27,50 @@ pub struct DatabaseSettings {
     pub require_ssl: bool,
 }
 
+impl DatabaseSettings {
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
+    }
+
+    pub fn with_db(&self) -> PgConnectOptions {
+        self.without_db().database(&self.database_name)
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let mut settings = config::Config::builder();
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
-
-    settings = settings
-        .add_source(config::File::from(configuration_directory.join("base")).required(true));
 
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
-        .expect("Failed to parse APP_ENVIRONMENT");
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    let environment_filename = format!("{}.yaml", environment.as_str());
+    let settings = config::Config::builder()
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
+        ))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
+        .build()?;
 
-    settings = settings.add_source(
-        config::File::from(configuration_directory.join(environment.as_str())).required(true),
-    );
-
-    settings = settings.add_source(config::Environment::with_prefix("app").separator("__"));
-
-    settings.build()?.try_deserialize()
+    settings.try_deserialize::<Settings>()
 }
 
 pub enum Environment {
@@ -68,36 +90,14 @@ impl Environment {
 impl TryFrom<String> for Environment {
     type Error = String;
 
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        match value.to_lowercase().as_str() {
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
             "local" => Ok(Self::Local),
             "production" => Ok(Self::Production),
             other => Err(format!(
-                "{} is not a supported environment. Use either 'local' or 'production'.",
+                "{} is not a supported environment. Use either `local` or `production`.",
                 other
             )),
         }
-    }
-}
-
-impl DatabaseSettings {
-    pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
-        options = options.log_statements(tracing::log::LevelFilter::Trace);
-        options
-    }
-
-    pub fn without_db(&self) -> PgConnectOptions {
-        let ssl_mode = if self.require_ssl {
-            PgSslMode::Require
-        } else {
-            PgSslMode::Prefer
-        };
-        PgConnectOptions::new()
-            .host(&self.host)
-            .username(&self.username)
-            .password(self.password.expose_secret())
-            .port(self.port)
-            .ssl_mode(ssl_mode)
     }
 }
